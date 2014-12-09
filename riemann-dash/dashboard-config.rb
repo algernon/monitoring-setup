87d455e1cdefcf478daef09d1ed6362701b14afe
Gergely Nagy <algernon@madhouse-project.org>
2014-12-09 09:42:45 +0100

    riemann-dash: Add a riemann-dash config file
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ riemann-dash/dashboard-config.rb
@@ -0,0 +1,9 @@
+# Copyright (C) 2014  Gergely Nagy <algernon@madhouse-project.org>
+#
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See the COPYING file for more details.
+
+set :port, 6003
+set :bind, "0.0.0.0"
+config[:ws_config] = 'dashboard.json'
