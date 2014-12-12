a25e6860c94c2b791de180197f56af7103216d3d
Gergely Nagy <algernon@madhouse-project.org>
2014-12-12 10:22:09 +0100

    riemann: Split the config up into smaller parts
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ riemann/inc/helpers.clj
@@ -0,0 +1,26 @@
+;; Copyright (C) 2014  Gergely Nagy <algernon@madhouse-project.org>
+;;
+;; This work is free. You can redistribute it and/or modify it under the
+;; terms of the Do What The Fuck You Want To Public License, Version 2,
+;; as published by Sam Hocevar. See the COPYING file for more details.
+
+(defmacro with-but-collectd
+  [keys & children]
+
+  (let [new-keys (merge {:host nil
+                         :type nil
+                         :type_instance nil
+                         :ds_type nil
+                         :ds_name nil
+                         :ds_index nil
+                         :plugin nil
+                         :plugin_instance nil}
+                        keys)]
+    `(~'with ~new-keys ~@children)))
+
+(defn float-to-percent
+  [& children]
+  (fn [e]
+    (when e
+      (let [new-event (assoc e :metric (* 100 (:metric e)))]
+        (call-rescue new-event children)))))
--- riemann/madhouse-project.org.clj
+++ riemann/madhouse-project.org.clj
@@ -7,73 +7,8 @@
 (require '[clojure.string :as string])
 (require '[org.spootnik.riemann.thresholds :refer [threshold-check]])
 
-(defmacro with-but-collectd
-  [keys & children]
-
-  (let [new-keys (merge {:host nil
-                         :type nil
-                         :type_instance nil
-                         :ds_type nil
-                         :ds_name nil
-                         :ds_index nil
-                         :plugin nil
-                         :plugin_instance nil}
-                        keys)]
-    `(~'with ~new-keys ~@children)))
-
-(defn float-to-percent
-  [& children]
-  (fn [e]
-    (when e
-      (let [new-event (assoc e :metric (* 100 (:metric e)))]
-        (call-rescue new-event children)))))
-
-(def thresholds
-  {"cpu-average/cpu-user" {:warning 30 :critical 60}
-   "cpu-average/cpu-system" {:warning 30 :critical 60}
-   "cpu-average/cpu-used" {:warning 60 :critical 90}
-   "cpu-average/cpu-nice" {:warning 50 :critical 20}
-   "cpu-average/cpu-idle" {:warning 50 :critical 20 :invert true}
-   "cpu-average/cpu-steal" {:warning 50 :critical 20}
-
-   "load/load/shortterm" {:warning 3 :critical 4}
-   "load/load/midterm" {:warning 3 :critical 4}
-   "load/load/longterm" {:warning 3 :critical 4}
-
-   "processes/ps_state-blocked" {:warning 4 :critical 8}
-   "processes/ps_state-paging" {:warning 4 :critical 8}
-   "processes/ps_state-running" {:warning 16 :critical 24}
-   "processes/ps_state-sleeping" {:warning 500 :critical 1000}
-   "processes/ps_state-stopped" {:warning 1 :critical 8}
-   "processes/ps_state-zombies" {:warning 0 :critical 8}
-
-   "memory/memory-buffered" {}
-   "memory/memory-cached" {}
-   "memory/memory-free" {:warning 0.10
-                         :critical 0.05
-                         :invert true}
-   "memory/memory-used" {}
-   "memory/percent-used" {:warning 80
-                          :critical 98}
-
-   "swap/swap-cached" {}
-   "swap/swap-free" {}
-   "swap/swap-used" {}
-   "swap/swap_io-in" {}
-   "swap/swap_io-out" {}
-
-   "uptime/uptime" {:warning 1 :critical 0 :invert true}
-
-   "df-root/percent_bytes-free" {:warning 10 :critical 6 :invert true}
-
-   "interface-wlan0/if_octets/rx" {:warning 768 :critical 1024}
-   "interface-wlan0/if_octets/tx" {:warning 768 :critical 1024}
-   "interface-eth0/if_octets/rx" {}
-   "interface-eth0/if_octets/tx" {}
-
-   "tail-auth/counter-sshd-invalid_user" {:warning 0 :critical 10}
-   "tail-auth/counter-sshd-successful-logins" {}
-   })
+(include "inc/helpers.clj")
+(include "inc/thresholds.clj")
 
 (logging/init :file "/srv/riemann/log/riemann.log"
               :console false)
